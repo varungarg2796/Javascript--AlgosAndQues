974d2e0007ae356bd039f62e28975c71d29a91e2
varungarg2796 <varungarg2796@hotmail.com>
2018-05-20 01:34:43 +0530

    longestWord

---
+++ FCC algos/longestWord.js
@@ -0,0 +1,16 @@
+
+
+function findLongestWord(str) {
+    var words = str.split(' ');
+    var maxLength = 0;
+
+    for (var i = 0; i < words.length; i++) {
+        if (words[i].length > maxLength) {
+            maxLength = words[i].length;
+        }
+    }
+
+    return maxLength;
+}
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
