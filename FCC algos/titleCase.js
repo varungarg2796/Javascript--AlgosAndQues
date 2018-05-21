e03e2da09ca9e164e361665212c04bcda777f68b
varungarg2796 <varungarg2796@hotmail.com>
2018-05-21 19:28:20 +0530

    titleCase

---
+++ FCC algos/titleCase.js
@@ -0,0 +1,15 @@
+
+//Return the provided string with the first letter of each word capitalized. 
+//Make sure the rest of the word is in lower case.
+
+function titleCase(str) {
+    var word = str.toLowerCase().split('');
+    var sentence = '';
+
+    for (var i = 0; i < str.length; i++) {
+        var sp = word[i].split();
+
+    }
+}
+
+titleCase("I'm a little tea pot");
